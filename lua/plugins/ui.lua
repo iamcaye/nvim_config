519e576664fba22625d50cc4c0a13453116f482c
Cayetano Biehler <cayetano.biehler@gmail.com>
2026-07-21 09:50:11 +0200

    fix: register snacks toggles without lazy event

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,24 +26,19 @@ require('snacks').setup({
   words = { enabled = true },
 })
 
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
-    Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
-    Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
-    Snacks.toggle.diagnostics():map('<leader>ud')
-    Snacks.toggle.line_number():map('<leader>ul')
-    Snacks.toggle.option('conceallevel', {
-      off = 0,
-      on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
-      name = 'Conceal Level',
-    }):map('<leader>uc')
-    Snacks.toggle.inlay_hints():map('<leader>uh')
-    Snacks.toggle.indent():map('<leader>ug')
-    Snacks.toggle.dim():map('<leader>uD')
-  end,
-})
+Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
+Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
+Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
+Snacks.toggle.diagnostics():map('<leader>ud')
+Snacks.toggle.line_number():map('<leader>ul')
+Snacks.toggle.option('conceallevel', {
+  off = 0,
+  on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
+  name = 'Conceal Level',
+}):map('<leader>uc')
+Snacks.toggle.inlay_hints():map('<leader>uh')
+Snacks.toggle.indent():map('<leader>ug')
+Snacks.toggle.dim():map('<leader>uD')
 
 vim.keymap.set('n', '<leader>z', function()
   Snacks.zen()
