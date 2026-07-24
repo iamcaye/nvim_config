53fcfaa1dd8f9e196bd0562442dc97563eb7aaa7
Cayetano Biehler <cayetano.biehler@gmail.com>
2026-07-24 12:34:39 +0200

    style: reformat plugin configs to 4-space indentation
    Also moves the lazygit keymap from gitsigns on_attach to a global
    mapping in ui.lua so it's available outside git-tracked buffers.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,29 +1,29 @@
 vim.cmd.colorscheme('ayu')
 
 require('which-key').setup({
-  delay = 0,
-  spec = {
-    { '<leader>a', group = 'AI' },
-    { '<leader>c', group = 'Code', mode = { 'n', 'x' } },
-    { '<leader>d', group = 'Document' },
-    { '<leader>g', group = 'Git' },
-    { '<leader>h', group = 'Hunk/Harpoon' },
-    { '<leader>s', group = 'Search' },
-    { '<leader>t', group = 'Terminal' },
-    { '<leader>u', group = 'UI toggles' },
-    { '<leader>w', group = 'Workspace' },
-  },
+    delay = 0,
+    spec = {
+        { '<leader>a', group = 'AI' },
+        { '<leader>c', group = 'Code',        mode = { 'n', 'x' } },
+        { '<leader>d', group = 'Document' },
+        { '<leader>g', group = 'Git' },
+        { '<leader>h', group = 'Hunk/Harpoon' },
+        { '<leader>s', group = 'Search' },
+        { '<leader>t', group = 'Terminal' },
+        { '<leader>u', group = 'UI toggles' },
+        { '<leader>w', group = 'Workspace' },
+    },
 })
 
 require('snacks').setup({
-  bigfile = { enabled = true },
-  lazygit = { enabled = true },
-  indent = { enabled = true },
-  input = { enabled = true },
-  notifier = { enabled = true },
-  quickfile = { enabled = true },
-  statuscolumn = { enabled = true },
-  words = { enabled = true },
+    bigfile = { enabled = true },
+    lazygit = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
 })
 
 Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
@@ -32,29 +32,33 @@ Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leade
 Snacks.toggle.diagnostics():map('<leader>ud')
 Snacks.toggle.line_number():map('<leader>ul')
 Snacks.toggle.option('conceallevel', {
-  off = 0,
-  on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
-  name = 'Conceal Level',
+    off = 0,
+    on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
+    name = 'Conceal Level',
 }):map('<leader>uc')
 Snacks.toggle.inlay_hints():map('<leader>uh')
 Snacks.toggle.indent():map('<leader>ug')
 Snacks.toggle.dim():map('<leader>uD')
 
 vim.keymap.set('n', '<leader>z', function()
-  Snacks.zen()
+    Snacks.zen()
 end, { desc = 'Toggle Zen Mode' })
 vim.keymap.set('n', '<leader>Z', function()
-  Snacks.zen.zoom()
+    Snacks.zen.zoom()
 end, { desc = 'Toggle Zoom' })
 vim.keymap.set('n', '<leader>.', function()
-  Snacks.scratch()
+    Snacks.scratch()
 end, { desc = 'Toggle Scratch Buffer' })
 vim.keymap.set('n', '<leader>S', function()
-  Snacks.scratch.select()
+    Snacks.scratch.select()
 end, { desc = 'Select Scratch Buffer' })
 vim.keymap.set('n', '<leader>n', function()
-  Snacks.notifier.show_history()
+    Snacks.notifier.show_history()
 end, { desc = 'Notification History' })
 vim.keymap.set('n', '<leader>bd', function()
-  Snacks.bufdelete()
+    Snacks.bufdelete()
 end, { desc = 'Delete Buffer' })
+
+vim.keymap.set('n', '<leader>gg', function()
+    Snacks.lazygit()
+end, { desc = 'Lazygit' })
