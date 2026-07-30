53fb3496818e27e569afca8c8bf8f8d06d1f792f
Cayetano Biehler <cayetano.biehler@topdigital.com>
2026-07-30 09:12:46 +0200

    chore: apply indentation and vim pack lock update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,64 +1,76 @@
-vim.cmd.colorscheme('ayu')
+vim.cmd.colorscheme("ayu")
 
-require('which-key').setup({
-    delay = 0,
-    spec = {
-        { '<leader>a', group = 'AI' },
-        { '<leader>c', group = 'Code',        mode = { 'n', 'x' } },
-        { '<leader>d', group = 'Document' },
-        { '<leader>g', group = 'Git' },
-        { '<leader>h', group = 'Hunk/Harpoon' },
-        { '<leader>s', group = 'Search' },
-        { '<leader>t', group = 'Terminal' },
-        { '<leader>u', group = 'UI toggles' },
-        { '<leader>w', group = 'Workspace' },
-    },
+require("which-key").setup({
+	delay = 0,
+	spec = {
+		{ "<leader>a", group = "AI" },
+		{ "<leader>c", group = "Code", mode = { "n", "x" } },
+		{ "<leader>d", group = "Document" },
+		{ "<leader>g", group = "Git" },
+		{ "<leader>h", group = "Hunk/Harpoon" },
+		{ "<leader>s", group = "Search" },
+		{ "<leader>t", group = "Terminal" },
+		{ "<leader>u", group = "UI toggles" },
+		{ "<leader>w", group = "Workspace" },
+	},
 })
 
-require('snacks').setup({
-    bigfile = { enabled = true },
-    lazygit = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+require("snacks").setup({
+	bigfile = { enabled = true },
+	lazygit = { enabled = true },
+	indent = { enabled = true },
+	input = { enabled = true },
+	notifier = { enabled = true },
+	quickfile = { enabled = true },
+	statuscolumn = { enabled = true },
+	words = { enabled = true },
+	explorer = { enabled = true },
+	picker = { enabled = true },
 })
 
-Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
-Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
-Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
-Snacks.toggle.diagnostics():map('<leader>ud')
-Snacks.toggle.line_number():map('<leader>ul')
-Snacks.toggle.option('conceallevel', {
-    off = 0,
-    on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
-    name = 'Conceal Level',
-}):map('<leader>uc')
-Snacks.toggle.inlay_hints():map('<leader>uh')
-Snacks.toggle.indent():map('<leader>ug')
-Snacks.toggle.dim():map('<leader>uD')
+Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+Snacks.toggle.diagnostics():map("<leader>ud")
+Snacks.toggle.line_number():map("<leader>ul")
+Snacks.toggle
+	.option("conceallevel", {
+		off = 0,
+		on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
+		name = "Conceal Level",
+	})
+	:map("<leader>uc")
+Snacks.toggle.inlay_hints():map("<leader>uh")
+Snacks.toggle.indent():map("<leader>ug")
+Snacks.toggle.dim():map("<leader>uD")
 
-vim.keymap.set('n', '<leader>z', function()
-    Snacks.zen()
-end, { desc = 'Toggle Zen Mode' })
-vim.keymap.set('n', '<leader>Z', function()
-    Snacks.zen.zoom()
-end, { desc = 'Toggle Zoom' })
-vim.keymap.set('n', '<leader>.', function()
-    Snacks.scratch()
-end, { desc = 'Toggle Scratch Buffer' })
-vim.keymap.set('n', '<leader>S', function()
-    Snacks.scratch.select()
-end, { desc = 'Select Scratch Buffer' })
-vim.keymap.set('n', '<leader>n', function()
-    Snacks.notifier.show_history()
-end, { desc = 'Notification History' })
-vim.keymap.set('n', '<leader>bd', function()
-    Snacks.bufdelete()
-end, { desc = 'Delete Buffer' })
+vim.keymap.set("n", "<leader>z", function()
+	Snacks.zen()
+end, { desc = "Toggle Zen Mode" })
+vim.keymap.set("n", "<leader>Z", function()
+	Snacks.zen.zoom()
+end, { desc = "Toggle Zoom" })
+vim.keymap.set("n", "<leader>.", function()
+	Snacks.scratch()
+end, { desc = "Toggle Scratch Buffer" })
+vim.keymap.set("n", "<leader>S", function()
+	Snacks.scratch.select()
+end, { desc = "Select Scratch Buffer" })
+vim.keymap.set("n", "<leader>n", function()
+	Snacks.notifier.show_history()
+end, { desc = "Notification History" })
+vim.keymap.set("n", "<leader>bd", function()
+	Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 
-vim.keymap.set('n', '<leader>gg', function()
-    Snacks.lazygit()
-end, { desc = 'Lazygit' })
+vim.keymap.set("n", "<leader>gg", function()
+	Snacks.lazygit()
+end, { desc = "Lazygit" })
+
+vim.keymap.set("n", "<leader>\\", function()
+	Snacks.explorer()
+end, { desc = "Toggle Explorer" })
+
+vim.keymap.set("n", "<leader>ep", function()
+	Snacks.picker()
+end, { desc = "Toggle Picker" })
